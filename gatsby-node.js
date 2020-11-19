449dacd464f40ba1c7ae2662d9778ca1858fd373
Frank Santaguida <fjs138@gmail.com>
2020-11-19 18:06:13 -0500

    blog template is creating pages dynamically based on markdown files in the source
    next goal:
    1 fetch slugs from posts
    2 use slug to generate a link to the post's page

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,11 +15,45 @@ module.exports.onCreateNode = ({ node, actions }) => {
     // console.log(JSON.stringify(node, undefined, 4)) // testing
     createNodeField({
       node,
-      name: 'slug',
-      value: slug
+      name: "slug",
+      value: slug,
     })
     // 1 node we're trying to create the node field on
     // 2 name of the field we are creating
     // 3 where the value of the value field comes from
   }
+}
+
+// generate pages
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions // deconstruct from actions
+  // 1 get path to template
+  const blogTemplate = path.resolve("./src/templates/blog.js")
+
+  // 2 get markdown data
+  const res = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  res.data.allMarkdownRemark.edges.forEach(edge => {  // 3 create new pages
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.fields.slug}`,
+      context: {
+        //things we can pass down to the template
+        slug: edge.node.fields.slug,
+      },
+    })
+  })
+
 }
