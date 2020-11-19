a55123c8397f8f5caff504efbcef797475b36b5e
Frank Santaguida <fjs138@gmail.com>
2020-11-19 13:24:39 -0500

    fixed contact page issue

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
 
 export default function ContactPage() {
   return (
-    <div>
-      <Layout />
+      <Layout>
       <h1>Contact Page</h1>
       <h2>
         The best way to reach me is by my personal email at{" "}
         <a href="mailto:fjs138@gmail.com">fjs138@gmail.com</a>
       </h2>
-      <Layout />
-    </div>
+      </Layout>
   )
 }
