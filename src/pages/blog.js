fad2726b8e61b17eb266d9af0596daf467029af6
Frank Santaguida <fjs138@gmail.com>
2020-11-19 22:26:28 -0500

    "final" main branch commit going to implement contentful

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import { Link, graphql, useStaticQuery } from "gatsby"
+import blogStyles from "./blog.module.scss"
 
 export default function BlogPage() {
   const data = useStaticQuery(graphql`
@@ -24,13 +25,13 @@ export default function BlogPage() {
   return (
     <Layout>
       <h1>Blog Page</h1>
-      <ol>
+      <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map(edge => {
           return (
-            <li>
-              < Link to={`/blog/${edge.node.fields.slug}`}>
-              <h2>{edge.node.frontmatter.title}</h2>
-              <p>{edge.node.frontmatter.date}</p>
+            <li className={blogStyles.post}>
+              <Link to={`/blog/${edge.node.fields.slug}`}>
+                <h2>{edge.node.frontmatter.title}</h2>
+                <p>{edge.node.frontmatter.date}</p>
               </Link>
             </li>
           )
