da116244400cb6125d130cd15d0015732ae55a97
Frank Santaguida <fjs138@gmail.com>
2026-05-13 22:18:43 -0400

    Update index.js
    update outdated home page content

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ export default function IndexPage() {
       <img className={"profilePicture disabledSelect"} src={profilePicture}/>
 
       <div id="greet">
-        A software engineer living in Philadelphia, I am currently available for
+        An AI software engineer from Philadelphia, now based in State College. I am currently available for
         hire.
         <br />
         <br />
