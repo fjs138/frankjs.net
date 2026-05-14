87289d59b8c92342f4ad551173084f907997da13
Frank Santaguida <fjs138@gmail.com>
2026-05-13 22:23:33 -0400

    Update me.js
    order of items

--- src/pages/me.js
+++ src/pages/me.js
@@ -8,59 +8,49 @@ export default function MePage() {
     <Layout>
       <Head title="Contact" />
       <h1>./Me</h1>
-      <h3>
-        ☎️ Phone:
-        {' '}
-        <Obfuscate tel="267-977-8356" />
-      </h3>
-      <h3>
-        📫 Email:
-        {' '}
-        <Obfuscate
-          email="fjs138@gmail.com"
-          headers={{
-            subject: 'Hi Frank!',
-            // cc: 'friend@coston.cool',
-          }}
-        />
-      </h3>
-      <h3>
-        🤝 Business:
-        {' '}
-        <a target="_blank" rel="noreferrer" href="https://builtbyfrank.com">
-          Built By Frank
-        </a>
-      </h3>
+     <h3>
+  ☎️ Phone:{' '}
+  <Obfuscate tel="267-977-8356" />
+</h3>
 
-      <h3>
-        👔
-        {' '}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.linkedin.com/in/franksantaguida/"
-        >
-          LinkedIn
-        </a>
-      </h3>
-      <h3>
-        🤖
-        {' '}
-        <a target="_blank" rel="noreferrer" href="https://github.com/fjs138">
-          Github
-        </a>
-      </h3>
-      <h3>
-        📝
-        {' '}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://builtbyfrank.com/Frank_Santaguida_Resume.pdf"
-        >
-          Resume
-        </a>
-      </h3>
+<h3>
+  📫 Email:{' '}
+  <Obfuscate
+    email="fjs138@gmail.com"
+    headers={{
+      subject: 'Hi Frank!',
+    }}
+  />
+</h3>
+
+<h3>
+  🔗 LinkedIn:{' '}
+  <a href="https://www.linkedin.com/in/frankjs" target="_blank" rel="noreferrer">
+    linkedin.com/in/frankjs
+  </a>
+</h3>
+
+<h3>
+  📄 Resume:{' '}
+  <a href="/resume.pdf" target="_blank" rel="noreferrer">
+    View Resume
+  </a>
+</h3>
+
+<h3>
+  💻 GitHub:{' '}
+  <a href="https://github.com/fjs138" target="_blank" rel="noreferrer">
+    github.com/fjs138
+  </a>
+</h3>
+
+<h3>
+  🏢 Business:{' '}
+  <a href="https://frankjs.net" target="_blank" rel="noreferrer">
+    frankjs.net
+  </a>
+</h3>
+
     </Layout>
   );
 }
